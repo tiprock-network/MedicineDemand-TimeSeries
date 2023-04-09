b0bf32785ddb729781e66a5f9ce16b2f15f6a37f
Theophilus <lincolntheop@gmail.com>
2023-04-09 22:36:37 +0300

    Removed single chatbot implementation

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, render_template, request
-from model import get_response,predict_class
 import json
 import joblib
 import pandas as pd
 app=Flask(__name__)
 
-#chatbot intents
-intents = json.loads(open("intents.json").read())
 
-#chatbot response route
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/get')
-def bot_response():
-    userTxt=request.args.get('msg')
-    
-    ints=predict_class(userTxt)
-    return get_response(ints,intents)
 
 
 @app.route('/mo1ab')
